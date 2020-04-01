59325669bbf2148f7cab3ee0de876e96cbaafe32
yangyifanNeu <yangyifanneu@163.com>
2020-04-01 17:10:45 +0800

    socket，动态图表

--- app.js
+++ app.js
@@ -31,13 +31,16 @@ app.post("/downloadImg", (_req, res) => {
   res.send("aaaa");
 });
 app.ws('/socketTest', function (ws, req){
+  if(ws.readyState == ws.OPEN){
+    ws.send(JSON.stringify(getDynamicData()));
+  }
   var sockedInterval = setInterval(()=>{
     if(ws.readyState !== ws.OPEN){
       clearInterval(sockedInterval);
     }else{
       ws.send(JSON.stringify(getDynamicData()));
     }
-  },2000);
+  },4000);
   
   ws.on('message', function (msg) {
       // 业务代码
